fd243134cdaf12ef13fbcaf577b0b47daaa63e86
DannyB <dannyb@telesofia.com>
2016-05-27 20:03:44 +0000

    improve examples readme

--- examples/04_element.rb
+++ examples/04_element.rb
@@ -6,7 +6,6 @@ svg = SVG.new
 svg.element :rect, x: 2, y: 2, width: 200, height: 200, fill: '#ddd'
 svg.rect x: 2, y: 2, width: 200, height: 200, fill: '#ddd'
 
-puts "Result:\n\n"
 p svg.content
 
 # => ["<rect x=\"2\" y=\"2\" width=\"200\" height=\"200\" fill=\"#ddd\"/>", 
