0f2f823081b62eeeb82a9877a2c28679c6d55e56
Danny Ben Shitrit <dannyb@telesofia.com>
2018-03-10 10:22:47 +0000

    fix some code smalls and add reek todo

--- lib/victor/css.rb
+++ lib/victor/css.rb
@@ -1,5 +1,6 @@
 module Victor
 
+  # Converts a Hash to a CSS string
   class CSS
     attr_reader :attributes
 
@@ -19,20 +20,30 @@ module Victor
       result = []
       hash.each do |key, value|
         key = key.to_s.tr '_', '-'
-        if value.is_a? Hash
-          result.push " " * indent + "#{key} {"
-          result.push convert_hash(value, indent+2)
-          result.push " " * indent + "}"
-        elsif value.is_a? Array
-          value.each do |row|
-            result.push " " * indent + "#{key} #{row};"
-          end
-        else
-          result.push " " * indent + "#{key}: #{value};"
-        end
+        result += css_block(key, value, indent)
       end
 
       result.join "\n"
+    end
+
+    def css_block(key, value, indent)
+      result = []
+
+      my_indent = " " * indent
+      
+      if value.is_a? Hash
+        result.push "#{my_indent}#{key} {"
+        result.push convert_hash(value, indent+2)
+        result.push "#{my_indent}}"
+      elsif value.is_a? Array
+        value.each do |row|
+          result.push "#{my_indent}#{key} #{row};"
+        end
+      else
+        result.push "#{my_indent}#{key}: #{value};"
+      end
+
+      result
     end
   end
 
