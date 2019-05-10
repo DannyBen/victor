6c347002ea0e04d5d5fd372752dbd6b2ba06a2d5
Danny Ben Shitrit <db@dannyben.com>
2019-05-10 07:32:17 +0000

    remove unused spec fixture function

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,12 +6,3 @@ require 'bundler'
 Bundler.require :default, :development
 
 include Victor
-
-def fixture(filename, data=nil)
-  if data
-    File.write "spec/fixtures/#{filename}", data
-    raise "Warning: Fixture data was written.\nThis is perfectly fine if it was intended,\nbut tests cannot proceed with it as a precaution."
-  else
-    File.read "spec/fixtures/#{filename}"
-  end
-end
