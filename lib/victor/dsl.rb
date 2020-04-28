503061a688c2376f937de97c72b74c4c17c98be4
Danny Ben Shitrit <db@dannyben.com>
2020-04-28 20:52:09 +0000

    forward append and element

--- lib/victor/dsl.rb
+++ lib/victor/dsl.rb
@@ -3,7 +3,7 @@ require 'forwardable'
 module Victor
   module DSL
     extend Forwardable
-    def_delegators :svg, :setup, :build, :save, :render
+    def_delegators :svg, :setup, :build, :save, :render, :append, :element
 
     def svg
       @svg ||= Victor::SVG.new
