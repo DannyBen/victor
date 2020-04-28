116358eae4ac88909831217d701ad4b1d51263c2
Danny Ben Shitrit <db@dannyben.com>
2020-04-28 16:44:58 +0000

    add dsl spec

---
+++ spec/victor/dsl_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe DSL do
+  subject { Class.new { include DSL }.new }
+
+  describe '#svg' do
+    it "returns a Victor::SVG.instance" do
+      expect(subject.svg).to be_a Victor::SVG
+    end
+  end
+
+  describe '#setup' do
+    it "forwards the call to the svg object" do
+      expect(subject.svg).to receive(:setup).with(attrib: :utes)
+      subject.setup attrib: :utes
+    end
+  end
+
+  describe '#build' do
+    it "forwards the call to the svg object" do
+      expect(subject.svg).to receive(:build)
+      subject.build
+    end
+  end
+
+  describe '#save' do
+    it "forwards the call to the svg object" do
+      expect(subject.svg).to receive(:save).with('filename')
+      subject.save 'filename'
+    end
+  end
+
+  describe '#render' do
+    it "forwards the call to the svg object" do
+      expect(subject.svg).to receive(:render)
+      subject.render
+    end
+  end
+
+end
