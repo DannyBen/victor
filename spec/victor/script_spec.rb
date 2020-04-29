597a9eff80092d7849edbd9f98bca9d290e9eecc
Danny Ben Shitrit <db@dannyben.com>
2020-04-29 06:17:54 +0000

    minor

--- spec/victor/script_spec.rb
+++ spec/victor/script_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'victor/script' do
   subject { File.read 'spec/fixtures/dsl-script.rb' }
 
-  it "includes Victor" do
+  it "includes Victor and Victor::DSL" do
     expect { eval subject }.to output("Victor::SVG\nVictor::SVG\ntrue\n").to_stdout
   end
 end
