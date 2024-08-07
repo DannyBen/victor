b1bef00ac4b9d157c08ca9b7692b9d8b07d9b556
Danny Ben Shitrit <db@dannyben.com>
2024-08-07 08:22:22 +0000

    update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,17 @@
 require 'simplecov'
-SimpleCov.start do
-  enable_coverage :branch
+unless ENV['NOCOV']
+  SimpleCov.start do
+    enable_coverage :branch if ENV['BRANCH_COV']
+    coverage_dir 'spec/coverage'
+    # track_files 'lib/**/*.rb'
+  end
 end
 
 require 'bundler'
 Bundler.require :default, :development
 
 include Victor
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = 'spec/status.txt'
+end
