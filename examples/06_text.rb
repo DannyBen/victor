fbd7ececbb2e52c33b71cda824e8887de06f1b53
Danny Ben Shitrit <db@dannyben.com>
2024-08-29 09:37:44 +0000

    remove examples

--- examples/06_text.rb
+++
@@ -1,8 +0,0 @@
-require 'victor'
-
-svg = Victor::SVG.new viewBox: '0 0 700 70'
-
-svg.rect x: 0, y: 0, width: 700, height: 70, fill: '#ddd'
-svg.text 'Victor', x: 100, y: 50, font_family: 'arial', font_weight: 'bold', font_size: 40, fill: 'blue'
-
-svg.save '06_text'
